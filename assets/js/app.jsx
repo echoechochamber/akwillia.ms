1ec031525563b71a8333dad82c2798165906145a
A. K. Williams <akwilliams@akwilliams.ibi.com>
2018-02-16 16:41:32 -0500

    added python 2.7 requireements, gunicorn, removed bundled from version control, add single start dev script

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 class App extends React.Component {
     render() {
-        return <h1>yo yo it's react here. for real dawg </h1>
+        return (
+            <div>
+                <h1>yo yo yo it's react here. for real dawg </h1>
+                <h2>--- for Real??</h2>
+                <h1> for really real dawg. </h1>
+                <h1> for reals. </h1>
+            </div>
+
+    )
     }
 };
 
---
+++ scripts/dev.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+HOME=`pwd`
+
+# start django with gunicorn
+gunicorn --bind=127.0.0.1:8080 akw_site.wsgi:application &
+# start the react frontend with
+yarn start
