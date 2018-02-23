7d51a720afa4a8c834049d1f250eabdea06463d3
A. K. Williams <akwilliams@akwilliams.fios-router.home>
2018-02-22 21:59:09 -0500

    basic site

--- akw_site/urls.py
+++ akw_site/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^favicon.ico$',RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'),permanent=False),name="favicon"),
     url(r'^$', TemplateView.as_view(template_name='staticpage.html'), name='home'),
-    # url(r'^$', TemplateView.as_view(template_name='main.html'), name='main'),
+    # url(r'^$', TemplateView.as_view(template_name='main.html'), name='main'),# later react version
     url(r'^admin/', admin.site.urls),
     url(r'^', include('api.urls')),
 ]
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-HOME=`pwd`
 
-# start django with gunicorn
-gunicorn --bind=127.0.0.1:8080 akw_site.wsgi:application &
-# start the react frontend with
-npm run start
+if [ -z $1 ]
+then
+    mode="static"
+elif [ -n $1 ]
+then
+    # otherwise, use the first arg to set the mode
+    mode=$1
+fi
+
+case $mode in
+    "static" )
+#        docker build -t djangotest . && docker run -p 0.0.0.0:8081:8081 djangotest;;
+        docker run -it  --mount type=bind,source="$(pwd)",target=/code -p 0.0.0.0:8081:8081 djangotest;;
+    "local" )
+        # start django with gunicorn
+        gunicorn --bind=127.0.0.1:8080 akw_site.wsgi:application &
+        # start the react frontend with
+        npm run start;;
+esac
